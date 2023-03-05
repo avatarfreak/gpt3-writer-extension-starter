deb447f3d2026cad7b2e777a3e6807f6a26e3d40
avatar <avatar@pop-os.localdomain>
2023-03-05 16:11:20 +0530

    Fix: updated white space

--- index.js
+++ index.js
@@ -3,29 +3,26 @@ const encode = (input) => {
 }
 
 const saveKey = () => {
-  // Grab text from input box
-  const input = document.getElementById("key_input");
+  const input = document.getElementById('key_input');
 
   if (input) {
     const { value } = input;
 
     // Encode String
-    const encodeValue = encode(value);
+    const encodedValue = encode(value);
 
     // Save to google storage
-    chrome.storage.local.set({ 'openai-key': encodeValue }, () => {
-      document.getElementById("key_needed").style.display = 'none'
-      document.getElementById("key_entered").style.display = 'block'
+    chrome.storage.local.set({ 'openai-key': encodedValue }, () => {
+      document.getElementById('key_needed').style.display = 'none';
+      document.getElementById('key_entered').style.display = 'block';
     });
   }
 };
 
 const changeKey = () => {
   document.getElementById('key_needed').style.display = 'block';
-  document.getElementById("key_entered").style.display = 'none';
-
-}
-
+  document.getElementById('key_entered').style.display = 'none';
+};
 
 const checkForKey = () => {
   return new Promise((resolve, reject) => {
@@ -35,15 +32,14 @@ const checkForKey = () => {
   });
 };
 
-checkForKey().then((response) =>{
-  if(response){
+checkForKey().then((response) => {
+  if (response) {
     document.getElementById('key_needed').style.display = 'none';
-    document.getElementById('key_entered').style.display='block';
+    document.getElementById('key_entered').style.display = 'block';
   }
-})
+});
 
-
-document.getElementById("save_key_button").addEventListener("click", saveKey);
-document.getElementById("change_key_button").addEventListener("click", changeKey);
+document.getElementById('save_key_button').addEventListener('click', saveKey);
+document.getElementById('change_key_button').addEventListener('click', changeKey);
 
 
